4af7bd88f3abaa4698f40f9957d4cd52f8a62d21
Panchal2003 <123099867+Panchal2003@users.noreply.github.com>
2024-07-26 15:45:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function check(){
     var input=document.getElementById('fname');
-    if(input.value !=" "){
+    // var input=document.getElementsById('fcourse');
+    if(input.value !== " "){
         ecertificate();
+
     }
     else{
         alert("input field empty");
     }
+    
+    
 }
 function ecertificate(){
     var x= document.getElementById("main");
@@ -14,4 +18,7 @@ function ecertificate(){
     var courses=document.getElementById("fcourse").value;
     document.getElementsByTagName('B')[0].innerHTML=cname;
     document.getElementsByTagName('h3')[0].innerHTML=courses;
+}
+function getdrt(){
+    document.getElementById('cstime').innerHTML=("Duration :<strong>"+ dtmc.ctime[dtmc.ctime.selectedIndex].text+"</strong>");
 }
